4482bf972cc7034a413a607afe42f2a2051a554b
domekisuzi <domekisuzi@163.com>
2024-06-25 20:29:34 +0800

    Reconfigure GitHub Actions for deployment

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,11 @@ import Markdown from 'unplugin-vue-markdown/vite'
 import MarkdownPreview from 'vite-plugin-markdown-preview'
 // https://vitejs.dev/config/
 export default defineConfig({
+      base: '/memory-love/',  // GitHub Pages 项目路径
+      build: {
+        outDir: 'dist'  // 确保这是构建输出目录
+      },
+
   plugins: [
     vue({
       include: [/.vue$/, /.md$/],
