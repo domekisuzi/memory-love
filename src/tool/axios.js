0ea58b4b6e86f9a60acc286f33a3b59b6643e777
domekisuzi <domekisuzi@163.com>
2024-10-05 18:46:28 +0800

    调试跨域2

--- src/tool/axios.js
+++ src/tool/axios.js
@@ -5,12 +5,12 @@ import axios from 'axios';
 export const instance = axios.create({
     baseURL: 'http://47.115.204.172:9876', // 根据实际情况修改后端服务器的地址
     timeout: 1000,
-    withCredentials: true,  // 允许跨域请求携带 Cookie
+    withCredentials: false,  //   不设置为false就会报错
 });
 
 export const collect = axios.create({
     baseURL: 'https://ip.011102.xyz', // 根据实际情况修改后端服务器的地址
     timeout: 1000,
-    withCredentials: true,  // 允许跨域请求携带 Cookie
+    withCredentials: false,  // 不设置为false就会报错
 });
 
