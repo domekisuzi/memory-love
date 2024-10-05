8cc0e706354e7b9c5067a655ec30022f64c2fecb
domekisuzi <domekisuzi@163.com>
2024-10-05 18:41:14 +0800

    修改baseURL，取消代理

--- src/tool/axios.js
+++ src/tool/axios.js
@@ -3,13 +3,13 @@
 import axios from 'axios';
 
 export const instance = axios.create({
-    baseURL: '/api', // 根据实际情况修改后端服务器的地址
+    baseURL: 'http://47.115.204.172:9876', // 根据实际情况修改后端服务器的地址
     timeout: 1000,
     withCredentials: true,  // 允许跨域请求携带 Cookie
 });
 
 export const collect = axios.create({
-    baseURL: '/collect', // 根据实际情况修改后端服务器的地址
+    baseURL: 'https://ip.011102.xyz', // 根据实际情况修改后端服务器的地址
     timeout: 1000,
     withCredentials: true,  // 允许跨域请求携带 Cookie
 });
--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,8 @@ import vue from '@vitejs/plugin-vue'
 import Markdown from 'unplugin-vue-markdown/vite'
 import MarkdownPreview from 'vite-plugin-markdown-preview'
 // https://vitejs.dev/config/
+
+// github pages是静态托管页面，不支持代理。 代理配置也只适用于开发调试
 export default defineConfig(
 
     {
@@ -27,21 +29,21 @@ export default defineConfig(
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-      server: {
-        proxy: {
-          '/api': {
-            //  http://localhost:9876
-            target: 'http://47.115.204.172:9876',
-            changeOrigin: true,
-            rewrite: (path) => path.replace(/^\/api/, ''),
-          },
-          '/collect':{
-            target: 'https://ip.011102.xyz',
-            changeOrigin: true,
-            rewrite: (path) => path.replace(/^\/ip/, ''),
-          }
-        },
-      },
+      // server: {
+      //   proxy: {
+      //     '/api': {
+      //       //  http://localhost:9876
+      //       target: 'http://47.115.204.172:9876',
+      //       changeOrigin: true,
+      //       rewrite: (path) => path.replace(/^\/api/, ''),
+      //     },
+      //     '/collect':{
+      //       target: 'https://ip.011102.xyz',
+      //       changeOrigin: true,
+      //       rewrite: (path) => path.replace(/^\/ip/, ''),
+      //     }
+      //   },
+      // },
 
 
 }
